15151d5989deecc588793e90af2225c447eaa8b3
Gabriel Souza <gablgdario03@gmail.com>
2023-09-18 16:27:49 -0300

    Init Project

---
+++ src/components/pages/Company.js
@@ -0,0 +1,11 @@
+import styles from '../pages/all.module.css'
+
+function Company(){
+    return(
+    <div className={styles.container}>
+        <h1>Company</h1>
+    </div>
+    )
+}
+
+export default Company
