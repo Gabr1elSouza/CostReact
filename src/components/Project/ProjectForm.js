586ea15299f1814463a5c7606b37d465fad7b3e3
Gabriel Souza <gablgdario03@gmail.com>
2023-09-24 00:10:12 -0300

    DB PART 1

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -6,10 +6,10 @@ import Input from '../Form/Input'
 import Select from '../Form/Select'
 import SubimitButton from '../Form/SubmitButton'
 
-function PorjectForm({btntext}){
+function ProjectForm({handleSubmit, btntext, projectData}){
 
     const [categories, setCategories]= useState([])
-
+    const [project, setProject]= useState(projectData|| {})
     useEffect(()=>{
         fetch("http://localhost:5000/categories",{
             method: 'GET',
@@ -23,13 +23,23 @@ function PorjectForm({btntext}){
         .catch((err)=> console.log(err))
     
     },[])
+
+    const submit = (e)=>{
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]:e.target.value})
+        console.log(project)
+    }
     return (
-    <form className={styles.form}>
-        <Input type='text' name='name' placeholder='Insira o nome do Projeto'/>
-        <Input type="number" name="bugets" text="Orçamento do Projeto"  placeholder="Digite o orçamento do projeto" /> 
+    <form onSubmit={submit} className={styles.form}>
+        <Input type='text' name='name' placeholder='Insira o nome do Projeto' handleOnChange={handleChange}/>
+        <Input type="number" name="bugets" text="Orçamento do Projeto"  placeholder="Digite o orçamento do projeto" handleOnChange={handleChange} /> 
         <Select name='categoty_id' text="Selecione a categoria" options={categories}/>
         <SubimitButton text={btntext} />
     </form>
     )
 }
-export default PorjectForm
+export default ProjectForm
