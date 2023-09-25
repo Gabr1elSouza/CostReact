9c4030fbeaeb6541a10eee7ebb94ae692ae5f802
Gabriel Souza <gablgdario03@gmail.com>
2023-09-25 16:46:36 -0300

    Message and Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,29 @@
+import Message from "../Layout/Message"
+import LinkButton from '../Layout/LinkButton'
+//hook
+import { useLocation } from "react-router-dom"
+
+import styles from './Projects.module.css'
+
 function Projects(){
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+        
+    }
     return (
-        <h1>Project</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text='Criar Projeo' ></LinkButton>
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <div className={styles.container}>
+            
+            </div>
+        </div>
     )
 }
 
