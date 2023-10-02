0473fde54b3e96085704312dbdb9d00b36bdea8c
Gabriel Souza <gablgdario03@gmail.com>
2023-10-02 14:49:57 -0300

    Details page

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,15 +1,18 @@
 import styles from './Project.module.css'
 
+import Loading from '../Layout/Loading'
+
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 function Project(){
     const {id} = useParams()
     
     const [project, setProject]= useState([])
+    const [showProjectForm, setShowProjectForm]= useState(false)
 
     useEffect(()=>{
-
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(()=>{
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -19,10 +22,45 @@ function Project(){
             setProject(data)
         })
         .catch((err)=> console.log(err))
+        },300)
+        
     },[id])
 
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
+
     return(
-        <p>{project.name}</p>
+       <>
+       {project.name ? (
+        <div className={styles.project_details}>
+            <div className={styles.details_container}>
+                <h1>Projeto: {project.name}</h1>
+                <button onClick={toggleProjectForm} className={styles.btn}>
+                    {!showProjectForm ? 'Editar Projeto' : "Fechar"}
+                </button>
+                {!showProjectForm? (
+                    <div className={styles.project_info}> 
+                        <p>
+                            <span>Categoria:</span> {project.category.name}
+                        </p>
+                        <p>
+                            <span>Total de Orçamento:</span>R${project.budget},00
+                        </p>
+                        <p>
+                            <span>Total utilizado:</span>R${project.cost},00
+                        </p>
+                    </div>
+                ):(<div className={styles.project_info}>
+                    <p>Detalhes do projeto</p>
+                    </div>)}
+            </div>
+        </div>
+        ): (
+        <Loading/>
+        
+    )}
+       </>
     )
 }
 
